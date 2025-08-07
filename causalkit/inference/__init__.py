a606e4b8b1b24a80a10f86ee13791c52645637fa
ioannmartynov <ioann.martynov2@gmail.com>
2025-08-07 22:24:19 +0300

    consolidated tests for `generate_rct_data`, `generate_obs_data`, and `CausalData.get_df` into a unified `test_data.py` for improved maintainability; renamed `causaldata` class to `CausalData` with stricter validations; updated imports and docs accordingly

--- causalkit/inference/__init__.py
+++ causalkit/inference/__init__.py
@@ -5,5 +5,9 @@ This module provides statistical inference tools for causal inference.
 """
 
 from causalkit.inference.ttest import ttest
+from causalkit.inference.ate.dml import dml as dml
+from causalkit.inference.ate.causalforestdml import causalforestdml as causalforestdml
+from causalkit.inference.att.dml import dml as dml_att
+from causalkit.inference.att.causalforestdml import causalforestdml as causalforestdml_att
 
-__all__ = ['ttest']
+__all__ = ['ttest', 'dml', 'causalforestdml', 'dml_att', 'causalforestdml_att']
